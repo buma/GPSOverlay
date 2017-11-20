27b304d8d23328911f233b29056301db5654b8e3
Marko Burjek <s0mebody.slo@gmail.com>
2017-11-20 19:19:49 +0100

    Test gauge is main script

--- GPSOverlay/test_gauge.py
+++ GPSOverlay/test_gauge.py
@@ -1,19 +1,21 @@
-from .GaugeClipMaker import GaugeClipMaker
+if __name__ == "__main__":
 
-duration=5 #seconds
-fps=25
+    from .GaugeClipMaker import GaugeClipMaker
+
+    duration=5 #seconds
+    fps=25
 
 
 
-import moviepy.editor as mpy
+    import moviepy.editor as mpy
 
-gcm = GaugeClipMaker("./gauges/velocimetro3.svg", 0, 260, 48, 312)
-def make_frame(t):
+    gcm = GaugeClipMaker("./gauges/velocimetro3.svg", 0, 260, 48, 312)
+    def make_frame(t):
 #Makes cursor move from 0-max in the time of this clip
-    speed = map_range(t, 0, duration, 0, 260)
-    #return gcm.make_clip(speed, 500, 500).make_frame(t)
-    return gcm.make_clip(speed).make_frame(t)
+        speed = map_range(t, 0, duration, 0, 260)
+        #return gcm.make_clip(speed, 500, 500).make_frame(t)
+        return gcm.make_clip(speed).make_frame(t)
 
-clip = mpy.VideoClip(make_frame, duration=duration) # 2 seconds
-clip.write_gif("svg2.gif",fps=fps)
+    clip = mpy.VideoClip(make_frame, duration=duration) # 2 seconds
+    clip.write_gif("svg2.gif",fps=fps)
 
